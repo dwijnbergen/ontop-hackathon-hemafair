969f700c6d3f9364b2b42850bdeb8da4da3dc7f8
dwijnbergen <j.d.wijnbergen1@gmail.com>
2026-04-17 13:34:12 +0200

    Made working example

--- create-db.py
+++ create-db.py
@@ -1,7 +1,7 @@
 import duckdb
 
-with duckdb.connect("example.db") as con:
+with duckdb.connect("artemis.db") as con:
     con.execute("""
-        CREATE TABLE IF NOT EXISTS example AS
-        SELECT * FROM read_csv_auto('example.csv')
+        CREATE TABLE IF NOT EXISTS artemis AS
+        SELECT * FROM read_csv_auto('artemis.csv')
     """)
