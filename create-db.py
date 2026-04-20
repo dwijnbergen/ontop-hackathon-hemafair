bc19363d2c1b222739581f289df362030b85090c
dwijnbergen <j.d.wijnbergen1@gmail.com>
2026-04-20 13:44:28 +0200

    Added mission and role table + started mapping

--- create-db.py
+++ create-db.py
@@ -2,6 +2,16 @@ import duckdb
 
 with duckdb.connect("artemis.db") as con:
     con.execute("""
-        CREATE TABLE IF NOT EXISTS artemis AS
-        SELECT * FROM read_csv_auto('artemis.csv')
+        CREATE TABLE IF NOT EXISTS astronauts AS
+        SELECT * FROM read_csv_auto('artemis_astronauts.csv')
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS missions AS
+        SELECT * FROM read_csv_auto('artemis_missions.csv')
+    """)
+
+    con.execute("""
+        CREATE TABLE IF NOT EXISTS roles AS
+        SELECT * FROM read_csv_auto('artemis_roles.csv')
     """)
