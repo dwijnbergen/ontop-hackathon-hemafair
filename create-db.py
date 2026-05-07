d831acf2316dd64ff54a609fb58764da077a9988
dwijnbergen <j.d.wijnbergen1@gmail.com>
2026-05-07 13:21:01 +0200

    create-db updates db in place + reports tables

--- create-db.py
+++ create-db.py
@@ -1,6 +1,18 @@
 import duckdb
 
-with duckdb.connect("artemis.db") as con:
+with duckdb.connect("input/artemis.db") as con:
+    con.execute("""
+        DROP TABLE astronauts
+    """)
+
+    con.execute("""
+        DROP TABLE missions
+    """)
+
+    con.execute("""
+        DROP TABLE roles
+    """)
+
     con.execute("""
         CREATE TABLE IF NOT EXISTS astronauts AS
         SELECT * FROM read_csv_auto('artemis_astronauts.csv')
@@ -15,3 +27,14 @@ with duckdb.connect("artemis.db") as con:
         CREATE TABLE IF NOT EXISTS roles AS
         SELECT * FROM read_csv_auto('artemis_roles.csv')
     """)
+
+    con.execute("""
+        SHOW ALL TABLES
+    """)
+
+    print("Current tables:")
+    for table in con.fetchall():
+        print("\033[1m" + table[2] + "\033[0m")
+        print("\t".join(table[3]))
+        print("\t".join(table[4]))
+        print("")
